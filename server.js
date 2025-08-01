d2e5e08e68f1ab5f280ba8f41b2d7f62cd935a40
TomSmaj <thomas.smajstrla@gmail.com>
2025-08-01 18:11:23 -0500

    bot returns error if user's chatId is not part of list present in .env

--- server.js
+++ server.js
@@ -3,17 +3,25 @@ import TelegramBot from "node-telegram-bot-api";
 
 const token = process.env.TELEGRAM_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
+const telegramChatIdList = process.env.ACCEPTED_TELEGRAM_CHAT_IDS.split(",");
 
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
   const messageText = msg.text;
-
-  if (messageText === "/start") {
-    bot.sendMessage(chatId, "Hi I'm PhaunaBot!");
+  if (!telegramChatIdList.includes(chatId.toString())) {
+    bot.sendMessage(
+      chatId,
+      '{\\"error":true,\\"status\\":500,\\"message\\":\\"Internal Server Error\\"}'
+    );
   } else {
-    bot.sendMessage(chatId, "Hey cool!");
+    if (messageText === "/start") {
+      console.log(msg);
+      bot.sendMessage(chatId, "Hi I'm PhaunaBot!");
+    } else {
+      bot.sendMessage(chatId, "Neat!");
+    }
   }
 });
 
 const app = express();
-app.listen(3002, () => console.log("PhaunaBot running on port 3000"));
+app.listen(3002, () => console.log("PhaunaBot running on port 3002"));
