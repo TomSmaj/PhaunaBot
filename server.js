a00ac1e2475f0e85ee0709fdf766e91539b1b863
TomSmaj <thomas.smajstrla@gmail.com>
2025-07-31 21:36:16 -0500

    Telegram bot functional

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 import express from "express";
+import TelegramBot from "node-telegram-bot-api";
+
+const token = process.env.TELEGRAM_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  const messageText = msg.text;
+
+  if (messageText === "/start") {
+    bot.sendMessage(chatId, "Hi I'm PhaunaBot!");
+  } else {
+    bot.sendMessage(chatId, "Hey cool!");
+  }
+});
 
 const app = express();
-app.listen(3002, () => console.log("Band bot server running on port 3000"));
+app.listen(3002, () => console.log("PhaunaBot running on port 3000"));
